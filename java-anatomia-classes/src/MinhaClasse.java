03c469a94dc111ce9245f9d9c1a49243b4aa5033
Ryan <ryan.j.moore35@gmail.com>
2024-05-04 11:56:18 -0400

    aula 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,9 +1,15 @@
 public class MinhaClasse {
     
 public static void main (String [] args) {
-    
-    System.out.print("Olá turm, sejam bem-vindos");
+    String primeiroNome = "Ryan";
+    String segundoNome = "Moore";
 
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    
+    System.out.println(nomeCompleto);
 }
 
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+}
 }
