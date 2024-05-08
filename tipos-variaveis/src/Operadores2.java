93678d63c1ab25b43cfb1e1274e370b6650e097a
Ryan <ryan.j.moore35@gmail.com>
2024-05-08 15:24:58 -0400

    aula 3

---
+++ tipos-variaveis/src/Operadores2.java
@@ -0,0 +1,13 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = -numero;
+
+        System.err.println(numero);
+
+        numero = numero * -1;
+
+        System.err.println(numero);
+    }
+}
