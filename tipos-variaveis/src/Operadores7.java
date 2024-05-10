f25e1bfd444b8bec308a95de73b337dbd051a3ef
Ryan <ryan.j.moore35@gmail.com>
2024-05-10 09:16:55 -0400

    aula 7

---
+++ tipos-variaveis/src/Operadores7.java
@@ -0,0 +1,19 @@
+public class Operadores7 {
+    public static void main(String[] args) {
+        // Operadores.java
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && ( 7 > 4 ) ){
+            System.out.println("as duas condicoes sao verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("uma das condicoes sao verdadeiras");
+        }
+
+        System.out.println("fim");
+
+    }
+}
