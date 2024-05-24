fe1baa5b22eece85bd8dcc8dfd092c81a26e6d0c
Ryan <ryan.j.moore35@gmail.com>
2024-05-24 19:44:52 -0400

    aula 3

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,15 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-        int nota = 6;
+        int nota = 10;
         
         if(nota >= 7)
-         System.out.println("Aprovado");
+            System.out.println("Aprovado");
  
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova Recuperação");
+
         else
-         System.out.println("Reprovado");
+            System.out.println("Reprovado");
      }
 }
