3e64880ab6a47b5aeb0db7e19262b170d1ecbc05
Ryan <ryan.j.moore35@gmail.com>
2024-07-06 06:51:09 -0400

    mudancas

--- VerificacaoNumeroContaBancaria/src/App.java
+++ VerificacaoNumeroContaBancaria/src/App.java
@@ -8,10 +8,8 @@ public class App {
             String numeroConta = scanner.nextLine();
 
             verificarNumeroConta(numeroConta);
-            // Caso nenhuma exceção seja lançada, imprime a mensagem de sucesso.
             System.out.println("Numero de conta valido.");
         } catch (IllegalArgumentException e) {
-            // TODO: Informar que o número de conta é inválido e exibir a mensagem de erro
             System.out.println("Erro: " + e.getMessage());
         } finally {
             // Fechar o scanner para evitar vazamentos de recursos
@@ -21,8 +19,6 @@ public class App {
 
     private static void verificarNumeroConta(String numeroConta) throws IllegalArgumentException {
         if (numeroConta.length() != 8) {
-            // TODO: Lançar uma IllegalArgumentException com a seguinte mensagem:
-            //System.err.println("Numero de conta invalido. Digite exatamente 8 digitos.");
             throw new IllegalArgumentException("Numero de conta invalido. Digite exatamente 8 digitos.");
         }
     }
