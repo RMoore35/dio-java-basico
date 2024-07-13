9b873924b59d7586fb29ff43faca980bd777373f
Ryan <ryan.j.moore35@gmail.com>
2024-07-13 17:15:00 -0400

    Primeiro commit

--- estruturas-excepcionais/src/AboutMe.java
+++ estruturas-excepcionais/src/AboutMe.java
@@ -1,8 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
+        try{
         //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
@@ -24,5 +26,9 @@ public class AboutMe {
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
         scanner.close();
+        }
+        catch (InputMismatchException e){
+            System.err.println("Os campos idade e altura precisam ser numéricos");
+        }
     }
 }
