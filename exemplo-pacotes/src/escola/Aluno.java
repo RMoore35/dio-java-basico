102a41cf0d8690f40e8163b7b017e49478b4ac04
Ryan <ryan.j.moore35@gmail.com>
2024-07-15 21:12:53 -0300

    aula 6

--- exemplo-pacotes/src/escola/Aluno.java
+++ exemplo-pacotes/src/escola/Aluno.java
@@ -1,6 +1,33 @@
 package escola;
 
 public class Aluno {
-    String nome;
-	int idade;
+    private String nome;
+	private int idade;
+
+    private String sexo;
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+	
 }
