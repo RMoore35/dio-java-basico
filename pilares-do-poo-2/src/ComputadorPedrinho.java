2852b32d5c31a66605b9805ab4f081cc76324556
Ryan <ryan.j.moore35@gmail.com>
2024-07-17 08:05:03 -0300

    aula 3

--- pilares-do-poo-2/src/ComputadorPedrinho.java
+++ pilares-do-poo-2/src/ComputadorPedrinho.java
@@ -1,14 +1,16 @@
 public class ComputadorPedrinho {
     public static void main(String[] args) {
-		//abrindo MSN Messenger
+		System.out.println("MSN");
 		MSNMessenger msn = new MSNMessenger();
 		msn.enviarMensagem();
 		msn.receberMensagem();
 
+        System.out.println("Facebook");
         FacebookMessenger fcb = new FacebookMessenger();
 		fcb.enviarMensagem();
 		fcb.receberMensagem();
 
+        System.out.println("Telegram");
         Telegram tlg = new Telegram();
 		tlg.enviarMensagem();
 		tlg.receberMensagem();
