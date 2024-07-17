b4f4a8720be27ea9af6867af5dcd64d17a19f61a
Ryan <ryan.j.moore35@gmail.com>
2024-07-17 17:43:25 -0300

    aula 6.4

--- pilares-do-poo-3/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
+++ pilares-do-poo-3/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -1,4 +1,22 @@
 package equipamentos.multifuncional;
-public class EquipamentoMultifuncional {
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
+    public void copiar() {
+        System.out.println("COPIANDO VIA EQUIPAMENTO MULTIFUNCIONAL");
+    }
+
+    public void digitalizar() {
+        System.out.println("DIGITALIZANDO VIA EQUIPAMENTO MULTIFUNCIONAL");
+    }
+
+    public void imprimir() {
+        System.out.println("IMPRIMINDO VIA EQUIPAMENTO MULTIFUNCIONAL");
+    }
+
+    
 
 }
--- pilares-do-poo-3/src/estabelecimento/loja.java
+++
@@ -1,4 +0,0 @@
-package estabelecimento;
-public class loja {
-
-}
