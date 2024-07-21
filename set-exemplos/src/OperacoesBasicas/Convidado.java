0078c52e31acae2c4c34c65859e0f0217b76acc1
Ryan <ryan.j.moore35@gmail.com>
2024-07-20 22:01:15 -0300

    inicializando class

---
+++ set-exemplos/src/OperacoesBasicas/Convidado.java
@@ -0,0 +1,23 @@
+package OperacoesBasicas;
+
+public class Convidado {
+    //atributos
+    private String nome;
+    private int codigoConvite;
+
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+}
