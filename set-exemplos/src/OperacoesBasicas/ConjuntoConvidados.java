11c71eccd719d8cfb5698b09d4b53dc30726da44
Ryan <ryan.j.moore35@gmail.com>
2024-07-20 22:37:50 -0300

    testando métodos

--- set-exemplos/src/OperacoesBasicas/ConjuntoConvidados.java
+++ set-exemplos/src/OperacoesBasicas/ConjuntoConvidados.java
@@ -34,5 +34,22 @@ public class ConjuntoConvidados {
     public void exhibirConvidados() {
         System.out.println(convidadoSet);
     }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+    
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPerCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.exhibirConvidados();
+    }
     
 }
--- set-exemplos/src/OperacoesBasicas/Convidado.java
+++ set-exemplos/src/OperacoesBasicas/Convidado.java
@@ -20,6 +20,34 @@ public class Convidado {
         return codigoConvite;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + codigoConvite;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Convidado other = (Convidado) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (codigoConvite != other.codigoConvite)
+            return false;
+        return true;
+    }
+
 
     @Override
     public String toString() {
