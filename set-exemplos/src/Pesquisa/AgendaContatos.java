6c49a739962dc23be6bd8318de4bb8260ab4f718
Ryan <ryan.j.moore35@gmail.com>
2024-07-21 13:56:39 -0300

    inicializando métodos

---
+++ set-exemplos/src/Pesquisa/AgendaContatos.java
@@ -0,0 +1,27 @@
+package Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributos
+    private Set<Contato> contatoSet;
+
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exhibirContatos() {
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato>
+    }
+
+}
---
+++ set-exemplos/src/Pesquisa/Contato.java
@@ -0,0 +1,58 @@
+package Pesquisa;
+import java.util.Objects;
+
+public class Contato {
+    //atributos
+    private String nome;
+    private int numero;
+
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getNumbero() {
+        return this.numero;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " nome='" + getNome() + "'" +
+            ", numero='" + getNumero() + "'" +
+            "}";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contato other = (Contato) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+
+}
